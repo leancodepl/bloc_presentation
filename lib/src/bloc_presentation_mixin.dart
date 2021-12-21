f60480fbd0607ba88aed34288072ddd045e995ee
shilangyu <xmarcinmarcin@gmail.com>
2021-12-21 21:42:07 +0100

    Add documentation

--- lib/src/bloc_presentation_mixin.dart
+++ lib/src/bloc_presentation_mixin.dart
@@ -4,17 +4,22 @@ import 'package:bloc/bloc.dart';
 import 'package:bloc_presentation/src/bloc_presentation_event.dart';
 import 'package:flutter/foundation.dart';
 
+/// [BlocPresentationMixin] adds a presentation stream to a [BlocBase]
+/// which is automatically disposed.
 mixin BlocPresentationMixin<S> on BlocBase<S> {
   final _presentationStream =
       StreamController<BlocPresentationEvent>.broadcast();
 
+  /// Stream emitting non-unique presentation events.
   Stream<BlocPresentationEvent> get presentation => _presentationStream.stream;
 
+  /// Emits a new presentation event.
   @protected
   void emitPresentation(BlocPresentationEvent event) =>
       _presentationStream.add(event);
 
   @override
+  @mustCallSuper
   Future<void> close() async {
     await _presentationStream.close();
     await super.close();
--- lib/src/use_bloc_presentation_listener.dart
+++ lib/src/use_bloc_presentation_listener.dart
@@ -2,6 +2,11 @@ import 'package:bloc_presentation/bloc_presentation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
+/// Subscribes to the presentation stream and invokes the provided listener
+/// on each event.
+///
+/// If [bloc] is omitted, [useBlocPresentationListener] will automatically perform
+/// a lookup using [Provider] and the current `BuildContext`.
 void useBlocPresentationListener<B extends BlocPresentationMixin<Object>>({
   required BlocPresentationWidgetListener listener,
   B? bloc,
