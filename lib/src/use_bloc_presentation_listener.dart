1fc66596dc9149738fad429342820fd0e3b94a48
shilangyu <xmarcinmarcin@gmail.com>
2021-12-22 08:39:50 +0100

    Update lib/src/use_bloc_presentation_listener.dart
    Co-authored-by: Bartek Pacia <barpac02@gmail.com>

--- lib/src/use_bloc_presentation_listener.dart
+++ lib/src/use_bloc_presentation_listener.dart
@@ -2,8 +2,7 @@ import 'package:bloc_presentation/bloc_presentation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
-/// Subscribes to the presentation stream and invokes the provided listener
-/// on each event.
+/// Subscribes to the presentation stream and invokes `listener` on each event.
 ///
 /// If [bloc] is omitted, [useBlocPresentationListener] will automatically perform
 /// a lookup using [Provider] and the current `BuildContext`.
