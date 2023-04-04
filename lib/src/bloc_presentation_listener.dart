8b6de32ae7ecabfb7d2979d284d7c45b99c304ca
Petr Nymsa <nymsa.petr@outlook.cz>
2023-04-04 20:34:45 +0200

    Fix #8 BlocPresentationListener has dynamic type argument

--- lib/src/bloc_presentation_listener.dart
+++ lib/src/bloc_presentation_listener.dart
@@ -18,8 +18,7 @@ typedef BlocPresentationWidgetListener = void Function(
 /// {@template bloc_presentation_listener}
 /// Widget that listens to new presentation events in a specified [bloc].
 /// {@endtemplate}
-class BlocPresentationListener<B extends BlocPresentationMixin<Object>>
-    extends SingleChildStatelessWidget {
+class BlocPresentationListener<B extends BlocPresentationMixin<dynamic>> extends SingleChildStatelessWidget {
   /// {@macro bloc_presentation_listener}
   const BlocPresentationListener({
     Key? key,
