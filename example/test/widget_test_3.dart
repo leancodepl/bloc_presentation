7c0d318469c083177a0a4c5f34aa002b5b1d3ede
Krzysztof Mamak <krzysztof.mamak@leancode.pl>
2023-05-15 17:27:06 +0200

    Add widget tests for all BlocPresentationMixin mocking variants

---
+++ example/test/widget_test_3.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+
+import 'package:bloc_presentation/bloc_presentation.dart';
+import 'package:bloc_presentation_example/comment_cubit.dart';
+import 'package:bloc_presentation_example/main.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockCommentCubit extends MockCubit<CommentState>
+    implements CommentCubit {}
+
+void main() {
+  late StreamController<BlocPresentationEvent> presentationController;
+  late MockCommentCubit commentCubit;
+
+  setUp(() {
+    presentationController = StreamController();
+    commentCubit = MockCommentCubit();
+  });
+
+  tearDown(() {
+    commentCubit.close();
+  });
+
+  Future<void> setupScreen(
+    WidgetTester tester, {
+    CommentState? initialState,
+    List<CommentState> states = const [],
+  }) async {
+    whenListen<CommentState>(
+      commentCubit,
+      Stream.fromIterable(states),
+      initialState: initialState ?? const CommentInitialState(),
+    );
+
+    whenListenPresentation3(commentCubit, presentationController);
+
+    await tester.pumpWidget(
+      _TestApp(
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider<CommentCubit>.value(value: commentCubit),
+          ],
+          child: const MyHomePage(),
+        ),
+      ),
+    );
+    await tester.pump();
+  }
+
+  group(
+    'MyHomePage',
+    () {
+      testWidgets(
+        'shows SnackBar with error message when cubit has emitted '
+        'FailedToUpvote presentation event',
+        (tester) async {
+          await setupScreen(
+            tester,
+            initialState: _commentReadyState,
+          );
+
+          presentationController.add(_failedToUpvoteEvent);
+
+          await tester.pumpAndSettle();
+
+          final snackBar = find.byType(SnackBar);
+
+          expect(snackBar, findsOneWidget);
+          expect(
+            find.descendant(
+              of: snackBar,
+              matching: find.textContaining(_failedToUpvoteEvent.reason),
+            ),
+            findsOneWidget,
+          );
+        },
+      );
+    },
+  );
+}
+
+class _TestApp extends StatelessWidget {
+  const _TestApp({
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: child,
+    );
+  }
+}
+
+const _commentReadyState = CommentReadyState('Content', 1, 0);
+const _failedToUpvoteEvent = FailedToUpvote('Bad connection');
