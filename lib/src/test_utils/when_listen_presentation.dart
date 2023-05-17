fe2c9b1fbc96a8768a6431ca625d8472d0e2def6
Krzysztof Mamak <krzysztof.mamak@leancode.pl>
2023-05-17 23:48:02 +0200

    Add another variant of mock class approach

--- lib/src/test_utils/when_listen_presentation.dart
+++ lib/src/test_utils/when_listen_presentation.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc_presentation/src/bloc_presentation_event.dart';
 import 'package:bloc_presentation/src/bloc_presentation_mixin.dart';
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mocktail/mocktail.dart';
 
 /// Approach 1.
@@ -88,9 +89,6 @@ void whenListenPresentation(
 
 /// Approach 6. - final
 ///
-/// We can also create our own _BlocBase equivalent to avoid presentation
-/// mocking duplication.
-///
 /// A mock for [BlocPresentationMixin]ed cubits. Allows managing presentation
 /// stream.
 class MockPresentationCubit<S> extends MockCubit<S>
@@ -122,6 +120,37 @@ class MockPresentationBloc2<E, S> extends MockBloc<E, S>
   final _presentationController = StreamController<BlocPresentationEvent>();
 
   /// Adds [event] to presentation stream
+  void emitMockPresentationEvent(BlocPresentationEvent event) {
+    _presentationController.add(event);
+  }
+}
+
+/// Another variant of 6. approach.
+///
+/// This variant allows us to get rid of bloc_test dependency and unnecessary
+/// duplication of presentation mocking.
+
+/// A mock for [BlocPresentationMixin]ed blocs. Allows managing presentation
+/// stream.
+class MockPresentationBlocX<E, S> extends _MockBlocPresentationBase<S>
+    implements Bloc<E, S> {}
+
+/// A mock for [BlocPresentationMixin]ed cubits. Allows managing presentation
+/// stream.
+class MockPresentationCubitX<S> extends _MockBlocPresentationBase<S>
+    implements Cubit<S> {}
+
+class _MockBlocPresentationBase<S> extends Mock
+    implements BlocPresentationMixin<S> {
+  _MockBlocPresentationBase() {
+    // stream mocking - as in _MockBlocBase from bloc_test package
+    when(() => stream).thenAnswer((_) => Stream<S>.empty());
+    when(() => presentation).thenAnswer((_) => _presentationController.stream);
+    when(close).thenAnswer((_) => _presentationController.close());
+  }
+
+  final _presentationController = StreamController<BlocPresentationEvent>();
+
   void emitMockPresentationEvent(BlocPresentationEvent event) {
     _presentationController.add(event);
   }
