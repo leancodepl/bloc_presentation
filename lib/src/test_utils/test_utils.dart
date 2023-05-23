bcc36b2266a2d3e462e3008246c972ba1559fbd7
Krzysztof Mamak <krzysztof.mamak@leancode.pl>
2023-05-23 20:57:22 +0200

    Remove unnecessary variant of class-based approach of mocking BlocPresentationMixin cubits / blocs

--- lib/src/test_utils/test_utils.dart
+++ lib/src/test_utils/test_utils.dart
@@ -1,3 +1,2 @@
 export 'package:bloc_presentation/src/test_utils/mock_presentation_bloc.dart';
-export 'package:bloc_presentation/src/test_utils/mock_presentation_bloc_2.dart';
 export 'package:bloc_presentation/src/test_utils/when_listen_presentation.dart';
