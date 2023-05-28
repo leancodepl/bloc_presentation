d2bdec18eb44ae700383b3501d43a08509c0b0c3
Krzysztof Mamak <krzysztof.mamak@leancode.pl>
2023-05-28 17:10:39 +0200

    Remove unnecessary files and exports

--- lib/bloc_presentation.dart
+++ lib/bloc_presentation.dart
@@ -1,5 +1,4 @@
 export 'src/bloc_presentation_event.dart';
 export 'src/bloc_presentation_listener.dart';
 export 'src/bloc_presentation_mixin.dart';
-export 'src/test_utils/test_utils.dart';
 export 'src/use_bloc_presentation_listener.dart';
--- lib/src/test_utils/test_utils.dart
+++
@@ -1,2 +0,0 @@
-export 'package:bloc_presentation/src/test_utils/mock_presentation_bloc.dart';
-export 'package:bloc_presentation/src/test_utils/when_listen_presentation.dart';
