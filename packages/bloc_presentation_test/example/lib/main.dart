6e06e6b28e7ad8c19043abb07d27417a96264eab
Krzysztof Mamak <krzysztof.mamak@leancode.pl>
2023-06-05 09:34:30 +0200

    Remove unnecessary files from bloc_presentation_test example, adjust example dependencies

--- packages/bloc_presentation_test/example/lib/main.dart
+++ packages/bloc_presentation_test/example/lib/main.dart
@@ -1,234 +1,87 @@
 import 'dart:async';
 
+import 'package:bloc/bloc.dart';
 import 'package:bloc_presentation/bloc_presentation.dart';
 import 'package:bloc_presentation_test/bloc_presentation_test.dart';
-import 'package:bloc_presentation_test_example/app.dart';
-import 'package:bloc_presentation_test_example/comment_cubit.dart';
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
-class MockCommentCubit1 extends MockPresentationCubit<CommentState>
-    implements CommentCubit {}
+class CounterCubit extends Cubit<int> with BlocPresentationMixin {
+  CounterCubit() : super(0);
+}
 
-class MockCommentCubit2 extends MockCubit<CommentState> //
-    implements
-        CommentCubit {}
+class CounterPresentationEvent extends BlocPresentationEvent {
+  const CounterPresentationEvent();
+}
+
+class MockCounterPresentationCubit extends MockPresentationCubit<int>
+    implements CounterCubit {}
+
+class MockCounterCubit extends MockCubit<int> implements CounterCubit {}
 
 void main() {
   mainMockPresentationCubit();
   mainWhenListenPresentation();
 }
 
-// MockPresentationCubit approach
 void mainMockPresentationCubit() {
-  late MockCommentCubit1 commentCubit;
+  late MockCounterPresentationCubit mockCubit;
 
   setUp(() {
-    commentCubit = MockCommentCubit1();
+    mockCubit = MockCounterPresentationCubit();
   });
 
   tearDown(() {
-    commentCubit.close();
+    mockCubit.close();
   });
 
-  Future<void> setupScreen(
-    WidgetTester tester, {
-    CommentState? initialState,
-    List<CommentState> states = const [],
-    List<BlocPresentationEvent>? presentationEvents,
-  }) async {
-    whenListen<CommentState>(
-      commentCubit,
-      Stream.fromIterable(states),
-      initialState: initialState ?? const CommentInitialState(),
-    );
+  test(
+    'presentation stream emits events properly',
+    () async {
+      mockCubit.emitMockPresentation(const CounterPresentationEvent());
 
-    await tester.pumpWidget(
-      _TestApp(
-        child: MultiBlocProvider(
-          providers: [
-            BlocProvider<CommentCubit>.value(value: commentCubit),
+      await expectLater(
+        mockCubit.presentation,
+        emitsInOrder(
+          <Matcher>[
+            equals(const CounterPresentationEvent()),
           ],
-          child: const HomePage(),
         ),
-      ),
-    );
-    await tester.pump();
-  }
-
-  group(
-    'HomePage',
-    () {
-      testWidgets(
-        'shows SnackBar with proper message when cubit has emitted '
-        'BlocPresentationEvent',
-        (tester) async {
-          await setupScreen(
-            tester,
-            initialState: _commentReadyState,
-          );
-
-          commentCubit.emitMockPresentation(_failedToUpvoteEvent);
-
-          await tester.pumpAndSettle();
-
-          final snackBar1 = find.byType(SnackBar);
-
-          expect(snackBar1, findsOneWidget);
-          expect(
-            find.descendant(
-              of: snackBar1,
-              matching: find.textContaining(_failedToUpvoteEvent.reason),
-            ),
-            findsOneWidget,
-          );
-
-          commentCubit.emitMockPresentation(_successfulUpvoteEvent);
-
-          await tester.pumpAndSettle();
-
-          final snackBar2 = find.byType(SnackBar);
-
-          expect(snackBar2, findsOneWidget);
-          expect(
-            find.descendant(
-              of: snackBar2,
-              matching: find.textContaining(_successfulUpvoteEvent.message),
-            ),
-            findsOneWidget,
-          );
-        },
       );
     },
   );
 }
 
-// whenListenPresentation approach
 void mainWhenListenPresentation() {
-  late StreamController<BlocPresentationEvent> presentationController;
-  late MockCommentCubit2 commentCubit;
+  late MockCounterCubit mockCubit;
+  late StreamController<CounterPresentationEvent> controller;
 
   setUp(() {
-    commentCubit = MockCommentCubit2();
+    mockCubit = MockCounterCubit();
+    controller = whenListenPresentation(
+      mockCubit,
+      initialEvents: [const CounterPresentationEvent()],
+    );
   });
 
   tearDown(() {
-    commentCubit.close();
+    mockCubit.close();
   });
 
-  Future<void> setupScreen(
-    WidgetTester tester, {
-    CommentState? initialState,
-    List<CommentState> states = const [],
-    List<BlocPresentationEvent>? presentationEvents,
-  }) async {
-    whenListen<CommentState>(
-      commentCubit,
-      Stream.fromIterable(states),
-      initialState: initialState ?? const CommentInitialState(),
-    );
+  test(
+    'presentation stream emits events properly',
+    () async {
+      controller.add(const CounterPresentationEvent());
 
-    presentationController = whenListenPresentation(
-      commentCubit,
-      initialEvents: presentationEvents,
-    );
-
-    await tester.pumpWidget(
-      _TestApp(
-        child: MultiBlocProvider(
-          providers: [
-            BlocProvider<CommentCubit>.value(value: commentCubit),
+      await expectLater(
+        mockCubit.presentation,
+        emitsInOrder(
+          <Matcher>[
+            equals(const CounterPresentationEvent()),
+            equals(const CounterPresentationEvent()),
           ],
-          child: const HomePage(),
         ),
-      ),
-    );
-    await tester.pump();
-  }
-
-  group(
-    'HomePage',
-    () {
-      testWidgets(
-        'shows SnackBar with proper message when cubit has emitted '
-        'BlocPresentationEvent',
-        (tester) async {
-          await setupScreen(
-            tester,
-            initialState: _commentReadyState,
-            presentationEvents: [_failedToUpvoteEvent],
-          );
-
-          await tester.pumpAndSettle();
-
-          final snackBar1 = find.byType(SnackBar);
-
-          expect(snackBar1, findsOneWidget);
-          expect(
-            find.descendant(
-              of: snackBar1,
-              matching: find.textContaining(_failedToUpvoteEvent.reason),
-            ),
-            findsOneWidget,
-          );
-
-          presentationController.add(_successfulUpvoteEvent);
-
-          await tester.pumpAndSettle();
-
-          final snackBar2 = find.byType(SnackBar);
-
-          expect(snackBar2, findsOneWidget);
-          expect(
-            find.descendant(
-              of: snackBar2,
-              matching: find.textContaining(_successfulUpvoteEvent.message),
-            ),
-            findsOneWidget,
-          );
-        },
-      );
-
-      // This test shows that presentation stream is automatically mocked even
-      // if presentation events have not been specified in
-      // whenListenPresentation (cubit's presentation getter returns empty
-      // stream by default).
-      testWidgets(
-        'does not SnackBar bar BlocPresentationEvent has not been emitted',
-        (tester) async {
-          await setupScreen(
-            tester,
-            initialState: _commentReadyState,
-          );
-
-          await tester.pumpAndSettle();
-
-          final snackBar = find.byType(SnackBar);
-
-          expect(snackBar, findsNothing);
-        },
       );
     },
   );
 }
-
-class _TestApp extends StatelessWidget {
-  const _TestApp({
-    required this.child,
-  });
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: child,
-    );
-  }
-}
-
-const _commentReadyState = CommentReadyState('Content', 1, 0);
-const _failedToUpvoteEvent = FailedToUpvote('Bad connection');
-const _successfulUpvoteEvent = SuccessfulUpvote('Successful upvote');
