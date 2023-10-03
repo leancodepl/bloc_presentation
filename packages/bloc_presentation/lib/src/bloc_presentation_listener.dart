2ed4c9fdb49e2752c221998df238494d604cfeec
Kamil Sztandur <kamil.sztandur@vp.pl>
2023-10-03 14:53:36 +0200

    Implement bloc presentation listener without hooks

--- packages/bloc_presentation/lib/src/bloc_presentation_listener.dart
+++ packages/bloc_presentation/lib/src/bloc_presentation_listener.dart
@@ -1,8 +1,8 @@
-import 'package:bloc_presentation/src/use_bloc_presentation_listener.dart';
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nested/nested.dart';
-import 'package:provider/provider.dart';
 
 import 'bloc_presentation_mixin.dart';
 
@@ -14,38 +14,133 @@ typedef BlocPresentationWidgetListener<P> = void Function(
   P event,
 );
 
-/// {@template bloc_presentation_listener}
-/// Widget that listens to new presentation events in a specified [bloc].
-/// {@endtemplate}
+/// A widget that listens to events from a Bloc and invokes a listener
+/// function in response to new events.
+///
+/// This widget is used to interact with [BlocPresentationMixin] and listen
+/// to events of type [P]. When a new event of type [P] is emitted by the Bloc,
+/// the provided [listener] function is called with the current [BuildContext]
+/// and the event itself.
+///
+/// Example:
+/// ```dart
+/// BlocPresentationListener<MyBloc, MyEvent>(
+///   listener: (context, event) {
+///     // Handle the event here
+///   },
+///   bloc: myBloc, // You don't have to pass it if you provided it in context
+///   child: SomeWidget(),
+/// )
+/// ```
 class BlocPresentationListener<B extends BlocPresentationMixin<dynamic, P>, P>
-    extends SingleChildStatelessWidget {
-  /// {@macro bloc_presentation_listener}
+    extends SingleChildStatefulWidget {
+  /// Creates a [BlocPresentationListener].
+  ///
+  /// The [listener] function is required and will be called with the
+  /// current [BuildContext] and the event of type [P] when new events are
+  /// emitted by the Bloc.
+  ///
+  /// The [bloc] parameter is optional and can be used to specify the
+  /// Bloc to listen to. If not provided, the nearest ancestor Bloc of
+  /// type [B] in the widget tree will be used.
+  ///
+  /// The [child] parameter is optional and represents the child widget
+  /// to display. If not provided, an empty [SizedBox] is used as the child.
   const BlocPresentationListener({
     super.key,
-    this.bloc,
     required this.listener,
-    super.child,
-  });
+    this.bloc,
+    this.child,
+  }) : super(child: child);
 
-  /// The [bloc] that the [BlocPresentationListener] will interact with.
-  /// If omitted, [BlocPresentationListener] will automatically perform a lookup using
-  /// [Provider] and the current `BuildContext`.
+  /// A function that defines the behavior when a new event of type [P] is
+  /// emitted by the Bloc. It takes the current [BuildContext] and the
+  /// event itself as parameters and is responsible for handling the event.
+  final BlocPresentationWidgetListener<P> listener;
+
+  /// The Bloc from which to listen to events of type [P]. If not provided,
+  /// the nearest ancestor Bloc of type [B] in the widget tree will be used.
   final B? bloc;
 
-  /// The [BlocPresentationWidgetListener] which will be called on every new presentation event.
-  final BlocPresentationWidgetListener<P> listener;
+  /// An optional child widget to display within the [BlocPresentationListener].
+  /// If not provided, an empty [SizedBox] is used as the child.
+  final Widget? child;
+
+  @override
+  SingleChildState<BlocPresentationListener<B, P>> createState() =>
+      _BlocPresentationListenerBaseState<B, P>();
+}
+
+class _BlocPresentationListenerBaseState<
+    B extends BlocPresentationMixin<dynamic, P>,
+    P> extends SingleChildState<BlocPresentationListener<B, P>> {
+  StreamSubscription<P>? _streamSubscription;
+  late B _bloc;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _bloc = _bloc = widget.bloc ?? context.read<B>();
+
+    _streamSubscription = _bloc.presentation.listen(
+      (event) => widget.listener(context, event),
+    );
+  }
+
+  @override
+  void didUpdateWidget(BlocPresentationListener<B, P> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    final oldBloc = oldWidget.bloc ?? context.read<B>();
+    final currentBloc = widget.bloc ?? oldBloc;
+
+    if (oldBloc != currentBloc) {
+      if (_streamSubscription != null) {
+        _unsubscribe();
+        _bloc = currentBloc;
+      }
+
+      _subscribe();
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final bloc = widget.bloc ?? context.read<B>();
+
+    if (_bloc != bloc) {
+      if (_streamSubscription != null) {
+        _unsubscribe();
+        _bloc = bloc;
+      }
+
+      _subscribe();
+    }
+  }
 
   @override
   Widget buildWithChild(BuildContext context, Widget? child) {
-    return HookBuilder(
-      builder: (context) {
-        useBlocPresentationListener(
-          listener: listener,
-          bloc: bloc,
-        );
+    return child ?? const SizedBox();
+  }
 
-        return child ?? const SizedBox();
-      },
+  @override
+  void dispose() {
+    _unsubscribe();
+
+    super.dispose();
+  }
+
+  void _subscribe() {
+    _streamSubscription = _bloc.presentation.listen(
+      (event) => widget.listener(context, event),
     );
+  }
+
+  void _unsubscribe() {
+    _streamSubscription?.cancel();
+    _streamSubscription = null;
   }
 }
